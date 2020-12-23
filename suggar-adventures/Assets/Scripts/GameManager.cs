20c4127197d1aad735978a672542c39a98e6b2ab
Tio Jota <jose.reyes03@alumnos.ucn.cl>
2020-12-22 23:32:56 -0300

    Prueba primer pisito

--- suggar-adventures/Assets/Scripts/GameManager.cs
+++ suggar-adventures/Assets/Scripts/GameManager.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject Piso1;
     public Renderer fondo;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Mapa Infinito
+        for (int i=0 ; i<21 ; i++){
+            Instantiate(Piso1, new Vector2(-10 + i,-4.5f), Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
