2051ffff25d88317885fdef853d39f11f68413f1
SebaBlamey <sebastian.cortes04@alumnos.ucn.cl>
2021-01-14 00:32:07 -0300

    La escena se reinicia cuando daddy muere, de momento solo funciona en seatle

--- suggar-adventures/Assets/Scripts/GameManager.cs
+++ suggar-adventures/Assets/Scripts/GameManager.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic;using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public enum GameState{Idle, Playing, Ended};
 
 public class GameManager : MonoBehaviour
 {
+    public GameState gameState = GameState.Idle;
    
     //El renderer hace alusión para poder realizar un update del fondo para que haga la ilusión indirectamente de mover el mapa
     public Renderer fondo;
@@ -19,6 +23,16 @@ public class GameManager : MonoBehaviour
         //En esta parte la funcion parte del fondo de nuestra escena haga la ilusion de mover el fondo
 
         fondo.material.mainTextureOffset = fondo.material.mainTextureOffset + new Vector2(0.005f, 0) * Time.deltaTime;
+        if (gameState == GameState.Ended)
+        {
+            ReiniciarJuego();
+        }
+    }
 
+    public void ReiniciarJuego()
+    {
+        SceneManager.LoadScene("Seatle");
     }
 }
+
+
