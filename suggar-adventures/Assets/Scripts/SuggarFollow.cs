5597f6e32f9d3e63d82330dba9a09735f3d42585
Tio Jota <jose.reyes03@alumnos.ucn.cl>
2021-01-14 11:41:33 -0300

    No sé que wea tengo que no me deja pulear

--- suggar-adventures/Assets/Scripts/SuggarFollow.cs
+++ suggar-adventures/Assets/Scripts/SuggarFollow.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 
 public class SuggarFollow : MonoBehaviour
 {
-    public GameObject follow;
+    public GameObject daddy;
+/*
+    public Transform Player;
+    private Vector3 vel = Vector3.zero;
+    public float Tiemsua = .15f;
+    */
+    
+ /*   public Vector2 minimo;
+    public Vector2 maximo;
+    public float suavizado;
+    Vector2 velocity;
+    */
     // Start is called before the first frame update
     void Start()
     {
@@ -12,11 +23,20 @@ public class SuggarFollow : MonoBehaviour
     }
 
     // Update is called once per frame
+    //Con esta función vamos a hacer que la camara siga a daddy pero que lo limite a ciertos rangos
     void Update()
     {
-        float posX = follow.transform.position.x;
-        float posY = follow.transform.position.y;
+        /*
+        Vector3 Pospla = Player.position;
+        Pospla.z = transform.position.z;
 
-        transform.position = new Vector3( transform.position.x, posY+4, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, Pospla, ref vel, Tiemsua);
+*/
+        /*
+        float posX = Mathf.SmoothDamp(transform.position.x, daddy.transform.position.x, ref velocity.x , suavizado);
+        float posY = Mathf.SmoothDamp(transform.position.y, daddy.transform.position.y, ref velocity.y, suavizado);
+
+        transform.position = new Vector3(Mathf.Clamp(posX,minimo.x,maximo.x),Mathf.Clamp(posY,minimo.y,maximo.y), transform.position.z) ;
+        */
     }
 }
